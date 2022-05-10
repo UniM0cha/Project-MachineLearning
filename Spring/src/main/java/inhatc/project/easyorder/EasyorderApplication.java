205aadb84808b89b2db66296e78ce949c2df5822
UniM0cha <solst_ice@naver.com>
2022-05-11 01:19:28 +0900

    0511 - 스프링 웹서버, 플라스크 서버 구출

---
+++ Spring/src/main/java/inhatc/project/easyorder/EasyorderApplication.java
@@ -0,0 +1,13 @@
+package inhatc.project.easyorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyorderApplication.class, args);
+	}
+
+}
---
+++ Spring/src/test/java/inhatc/project/easyorder/EasyorderApplicationTests.java
@@ -0,0 +1,13 @@
+package inhatc.project.easyorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
