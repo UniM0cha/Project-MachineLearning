c366685f20a9c92e19e983cd002c1c3ec43f9124
UniM0cha <solst_ice@naver.com>
2022-06-02 09:17:16 +0900

    0602 - 플라스크 기능구현중...

--- Flask/predict.py
+++ Flask/predict.py
@@ -1,9 +1,19 @@
 import pandas as pd
+from prophet import Prophet
 
 
 def predict_next_week(storeId, productId):
     data = pd.read_csv('./sales_modified.csv', index_col=0)
     data = data[(data['store_id'] == storeId) &
                 (data['product_id'] == productId)]
+
+    prophet = Prophet()
+    prophet.fit(data)
+
+    future = prophet.make_future_dataframe(periods=7, freq='D')
+
+    forecast = prophet.predict(future)
+
+    prophet.plot(forecast)
     print(data)
     return data
