785d878e0b4143fb4bb05b574a7cf0c9156be369
UniM0cha <solst_ice@naver.com>
2022-06-13 02:03:13 +0900

    0613 - 날짜 데이터 형식 변경

--- Flask/predict.py
+++ Flask/predict.py
@@ -12,15 +12,15 @@ def auto_order_list(store_id):
     pred_sum_list = []
     pred_list = []
     for product_id in db_result_df['product_id']:
-        pred = pd.read_csv('data/pred_csv/pred_' +
-                           str(store_id) + '-' + str(product_id) + '.csv')
+        pred = pd.read_csv('data/pred_csv/pred_' + str(store_id) + '-' +
+                           str(product_id) + '.csv')
         pred_sum_list.append(sum(pred['yhat'].iloc[21:]))
         pred_list.append(pred)
 
-    db_result_df['pred_sum'] = pd.DataFrame(
-        pred_sum_list, columns=['pred_sum'])
-    db_result_df = db_result_df.sort_values(
-        by='pred_sum', ascending=False).head(5)
+    db_result_df['pred_sum'] = pd.DataFrame(pred_sum_list,
+                                            columns=['pred_sum'])
+    db_result_df = db_result_df.sort_values(by='pred_sum',
+                                            ascending=False).head(5)
     db_result_df['pred_sum'] = db_result_df['pred_sum'].round()
 
     pred_top5_list = []
@@ -33,13 +33,10 @@ def auto_order_list(store_id):
     date_list = []
     pred_sale_list = []
     for pred in pred_top5_list:
-        date_list.append(pred['ds'].to_list())
-        pred_sale_list.append(pred['yhat'].to_list())
+        date_list.append(dict(date=pred['ds'].to_list()))
+        pred_sale_list.append(dict(predict=pred['yhat'].to_list()))
 
-    chart_data = dict(
-        상품명=product_name_list,
-        날짜=date_list,
-        판매량=pred_sale_list)
+    chart_data = dict(상품명=product_name_list, 날짜=date_list, 판매량=pred_sale_list)
 
     ############### table_data를 만드는 과정 #################
     order = db_result_df['pred_sum'] - db_result_df['stock']
@@ -48,7 +45,4 @@ def auto_order_list(store_id):
 
     db_result_df_tolist = db_result_df.values.tolist()
 
-    return {
-        "chart_data": chart_data,
-        "table_data": db_result_df_tolist
-    }
+    return {"chart_data": chart_data, "table_data": db_result_df_tolist}
