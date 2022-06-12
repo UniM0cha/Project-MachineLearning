c9f0457f59252784c01ecd243b159e8987e99588
MinJu-A <pinokoiandmj@naver.com>
2022-06-13 04:17:41 +0900

    서버 연결 부분 안 되는 거 -  main 확인하면 됨

--- eAsy_Order/eo_main/src/page/EoLogin.js
+++ eAsy_Order/eo_main/src/page/EoLogin.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { findAllInRenderedTree } from "react-dom/test-utils";
 
+// route
+// import { useHistory } from "react-router";
+import EoMain from "./EoMain";
 
 
 
 const Login = () => {
+    const onLogin = () => {
+        // const history = useHistory();
+        let id = document.querySelector("#id");
+        let pw = document.querySelector("#pw");
+
+        if(id.value === "" || pw.value === ""){
+            alert("id/pw가 입력이 되지 않았습니다.")
+        }else if(id.value !== "admin" || pw.value !== "1234"){
+            alert("회원이 아닙니다.")
+        }else{
+            alert("어서오세요")
+          
+        }
+    }
 
     return (
         <div className="body"
@@ -28,12 +46,12 @@ const Login = () => {
           
             <div>
             <form style={{ display: 'flex', flexDirection: 'column', width: '300px', verticalAlign: "middle" }} >
-                <label className="font-white">Email</label>
-                <input type="id" />
+                <label className="font-white">ID</label>
+                <input type="id" id ="id"/>
                 <label className="font-white">Password</label>
-                <input type="pw"  />
+                <input type="password"  id = "pw"/>
                 <br />
-                <button type="submit" className="button" >Login</button>
+                <button type="submit" className="button" onClick={onLogin} href="/main">Login</button>
             </form>
             </div>
         </div>
